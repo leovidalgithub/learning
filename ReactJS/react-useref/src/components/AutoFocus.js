c526ec727521478a60380f63da16b3aa5c523cc9
Leonardo Rdgz-Vidal <leo@leovidal.es>
2023-03-19 02:08:38 -0400

    Mar 18 2023

--- ReactJS/react-useref/src/components/AutoFocus.js
+++ ReactJS/react-useref/src/components/AutoFocus.js
@@ -1,25 +1,24 @@
 import React, { useRef } from "react";
 
 const AutoFocus = () => {
-	const inputRef = useRef(null);
+	const inputRef1 = useRef(null);
+	const inputRef2 = useRef(null);
 
-	const handleOnFocus = e => {
-		inputRef.current.focus();
-		inputRef.current.style.border = '5px solid crimson';
-		inputRef.current.addEventListener('blur', function(e) {
-			inputRef.current.style.border = 'none';
+	const handleOnFocus1 = e => {
+		inputRef1.current.focus();
+		inputRef1.current.style.border = '5px solid crimson';
+		inputRef1.current.addEventListener('blur', function(e) {
+			inputRef1.current.style.border = 'none';
 		})
 	}
 
 	return (
 		<div>
 			<hr />
-			<input type="text" ref={inputRef} />&nbsp;
-			<button
-				onClick={handleOnFocus}
-			>
-				Input focus
-			</button>
+			<input type="text" ref={inputRef1} />&nbsp;
+			<button onClick={handleOnFocus1}>Input focus()</button><br/>
+			<input type="text" ref={inputRef2} />&nbsp;
+			<button onClick={() => inputRef2.current.select()}>Input select()</button>
 		</div>
 	)
 }
